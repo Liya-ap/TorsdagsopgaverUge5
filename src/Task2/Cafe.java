334a8f590a59248f38689a1d916e5b70d98971f4
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2023-10-04 10:35:44 +0200

    Task 2: Load coffee menu for a cafe

--- src/Task2/Cafe.java
+++ src/Task2/Cafe.java
@@ -1,4 +1,28 @@
 package src.Task2;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+
 public class Cafe {
+    private ArrayList<String> coffeeMenu = new ArrayList<>();
+
+    public ArrayList<String> getCoffeeMenu() {
+        return coffeeMenu;
+    }
+
+    public void loadMenuData() {
+        File file = new File("src/Task2/coffees.txt");
+
+        try {
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                String coffee = fileReader.nextLine();
+                coffeeMenu.add(coffee);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+        }
+    }
 }
--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,4 +1,12 @@
 package src.Task2;
 
 public class Main {
+    public static void main(String[] args) {
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
+
+        for (String coffee: cafe.getCoffeeMenu()) {
+            System.out.println(coffee);
+        }
+    }
 }
